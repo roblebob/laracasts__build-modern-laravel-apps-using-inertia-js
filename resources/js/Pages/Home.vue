8b2ea2e4121145a1aa9872eb2f3690037e10a295
roblebob <robert.koppisch@gmail.com>
2025-01-06 14:00:33 +0100

    1. .. 3. DONE

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,25 @@
+<script>
+    export default {
+        props: {
+            name: String,
+            frameworks: Array,
+        },
+    };
+
+</script>
+
+<template>
+    <div>
+        <h1>Welcome, {{ name }}</h1>
+
+        <p>In this series, we use </p>
+
+        <ul>
+            <li v-for="framework of frameworks"  v-text="framework"></li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return inertia('Welcome');
+    //return inertia('Welcome');
+    return Inertia::render('Home', [
+        'name' => 'John Doe',
+        'frameworks' => ['Laravel', 'Vue', 'Inertia']
+    ]);
 });
