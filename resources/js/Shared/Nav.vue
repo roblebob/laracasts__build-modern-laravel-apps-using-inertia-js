f278fdf4281f00d91ad44ba4eb2d99f205a7f2b7
roblebob <robert.koppisch@gmail.com>
2025-01-07 10:42:07 +0100

    8. Active Links  DONE

--- resources/js/Shared/Nav.vue
+++ resources/js/Shared/Nav.vue
@@ -1,8 +1,10 @@
 <script>
 import { Link } from '@inertiajs/vue3';
+import NavLink from "./NavLink.vue";
 
 export default {
     components: {
+        NavLink,
         Link,
     },
 };
@@ -14,10 +16,11 @@ export default {
 <template>
     <nav class="mt-6">
         <ul class="list-disc">
-            <li><Link href="/" class="text-blue-500 hover:underline">Home</Link></li>
-            <li><Link href="/users" class="text-blue-500 hover:underline">Users</Link></li>
-            <li><Link href="/settings" class="text-blue-500 hover:underline">Settings</Link></li>
-            <li><Link href="/logout" method="post" class="text-blue-500 hover:underline" :data="{ foo: 'bar'}">Log Out</Link></li>
+            <li><NavLink href="/"       :active="$page.url === '/'" >Home</NavLink></li>
+
+            <li><NavLink href="/users"  :active="$page.url.startsWith('/users')" >Users</NavLink></li>
+
+            <li><NavLink href="/settings" :active="$page.component === 'Settings'" >Settings</NavLink></li>
         </ul>
     </nav>
 </template>
---
+++ resources/js/Shared/NavLink.vue
@@ -0,0 +1,22 @@
+<script>
+import {Link} from "@inertiajs/vue3";
+export default {
+    components: { Link, },
+    props: {
+        active: Boolean,
+    }
+};
+</script>
+
+
+<template>
+    <Link class="text-blue-500 hover:underline"
+          :class="{ 'font-bold underline': active }"
+    ><slot /></Link>
+</template>
+
+
+
+<style scoped>
+
+</style>
